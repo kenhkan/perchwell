6eba2694d2bb074ca64240c413a8512e91f060eb
Kenneth Kan <ken@pixbi.com>
2015-04-20 13:35:10 -0400

    Use Google Maps API

---
+++ gmap.js
@@ -0,0 +1,44 @@
+(function () {
+  var iw = new google.maps.InfoWindow();
+  var coordinates = new Array();
+  var markers = new Array();
+
+  // Manhattan coordinates
+  var coords = new google.maps.LatLng(40.783435, -73.966249);
+
+  var mapOptions = {
+    zoom: 13,
+    center: coords,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var mapDom = document.getElementById("map");
+  var map = new google.maps.Map(mapDom, mapOptions);
+
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.POLYGON,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: [google.maps.drawing.OverlayType.POLYGON]
+    },
+    polygonOptions: {
+      editable: true
+    }
+  });
+
+  drawingManager.setMap(map);
+
+  google.maps.event.addListener(drawingManager, "overlaycomplete", function(event) {
+    var i, l, c;
+    var points = [];
+    var coords = event.overlay.getPath().getArray();
+
+    for (i = 0, l = coords.length; i < l; i++) {
+      c = coords[i];
+      // Coordinates as tuples
+      points.push([c.k, c.D]);
+    }
+
+    console.log("polygon", points);
+  });
+})();
